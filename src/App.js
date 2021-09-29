83b64c1e5cd27bc4e0a80621674b402987814236
Bhautik Patel <bhautikpatel8494@gmail.com>
2021-09-29 15:29:27 +0530

    Test changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import History from "./component/History/History";
 import About from "./component/About/About";
 import StudentHistory from "./component/StudentHistory/StudentHistory";
 
+console.log(`object`)
 function App() {
   return (
     <>
