34168a3278bdc341d56150f3b165beaa092b6a4c
Bhautik Patel <bhautikpatel8494@gmail.com>
2021-10-04 10:29:54 +0530

    code reFactor

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Login from "./component/Login/Login";
 import Register from "./component/Register/Register";
 import Home from "./component/Home/Home";
 import Mcq from "./component/Mcq/Mcq";
-import PrivateRoute from "./component/PrivateRoute";
+import PrivateRoute from "./utils/PrivateRoute";
 import History from "./component/History/History";
 import About from "./component/About/About";
 import StudentHistory from "./component/StudentHistory/StudentHistory";
 import Addmission from "./component/Addmission/Addmission";
+import AddmissionInfo from "./component/Addmission/AddmissionInfo";
+import AddExam from "./component/AddExam/AddExam";
+import UpdateGiveExam from "./component/GiveExam/UpdateGiveExam";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-multi-carousel/lib/styles.css";
-import AddmissionInfo from "./component/Addmission/AddmissionInfo";
-import AddExam from "./component/AddExam/AddExam";
-import UpdateGiveExam from "./component/GiveExam/UpdateGiveExam";
 
 function App() {
   return (
